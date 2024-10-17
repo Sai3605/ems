53731604d9ddc7adeeea6a769f02def43acd1140
Sai3605 <22eg106a42@anurag.edu.in>
2024-10-17 17:12:20 +0530

    Update main.js

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,9 +1,9 @@
 module.exports.index=function(req,res){
-    res.render('index', { title: 'Sai Charan' });
+    res.render('index', { title: 'Home' });
    };
    module.exports.signin=function(req,res){
-    res.render('signin', { title: 'Sai Charan' });
+    res.render('signin', { title: 'Sign In' });
    };
    module.exports.review=function(req,res){
-    res.render('review', { title: 'Sai Charan' });
+    res.render('review', { title: 'Review' });
    };
